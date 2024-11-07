852f06949c9760b62dc234b28604658038a54e8c
trickkyricky <rickygyoungjr@gmail.com>
2024-11-06 19:27:30 -0600

    spring initializer project

---
+++ src/test/java/com/alerts/alerts/AlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerts.alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
